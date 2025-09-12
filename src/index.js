8e5e152fdf1685cfb05faff2e202baf741642c1d
naman3309 <nggoel888@gmail.com>
2025-09-12 18:32:15 +0530

    DB connection established

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './db/db.connect.js'
+
+dotenv.config({path: './env'})
+
 const app = express()
+const port = process.env.PORT || 5000
+
+connectDB()
+
+app.listen(port, () => {
+    console.log(`Server started on PORT ${port}`)
+})
+
+
